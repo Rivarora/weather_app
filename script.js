e0d7ea442d5c5a932126e26d64f5994a47f5c6c4
Riva <riva0697.becse24@chitkara.edu.in>
2025-04-14 13:24:48 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "893971476f90297c35a0aab354c27d75"; // Your API key
+const apiKey = "893971476f90297c35a0aab354c27d75"; // Replace with your API key
 
 const quotes = [
   "Chase the sunshine, even on cloudy days.",
@@ -17,26 +17,4 @@ function getWeather() {
     ? `https://api.openweathermap.org/data/2.5/weather?zip=${input},IN&appid=${apiKey}&units=metric`
     : `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
 
-  fetch(apiUrl)
-    .then(res => {
-      if (!res.ok) throw new Error("Location not found");
-      return res.json();
-    })
-    .then(data => {
-      displayWeather(data);
-    })
-    .catch(error => alert("Error fetching weather: " + error.message));
 }
-
-function displayWeather(data) {
-  document.getElementById("weatherBox").classList.remove("hidden");
-  document.getElementById("location").textContent = `${data.name}, ${data.sys.country}`;
-  document.getElementById("temp").textContent = `🌡️ ${data.main.temp} °C`;
-  document.getElementById("desc").textContent = `🔎 ${data.weather[0].description}`;
-  document.getElementById("icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  document.getElementById("quote").textContent = quotes[Math.floor(Math.random() * quotes.length)];
-}
-
-
-  
-      
